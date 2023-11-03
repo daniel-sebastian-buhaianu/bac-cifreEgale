96b8469e52270bab66faeef19087451d669cd5df
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-11-03 15:02:17 +0200

    imbunatateste solutia

--- cifreEgale.cpp
+++ cifreEgale.cpp
@@ -3,64 +3,27 @@
 #include <cmath>
 using namespace std;
 ofstream fout("bac.out");
-int nrCifre(int);
 int main()
 {
-	int a, b, i, nrca, nrcb, pca, pcb, xa, xb, cif, nrc;
+	int a, b, nra, na, i;
 	cin >> a >> b;
-	nrca = nrCifre(a); // nr. cifre a lui a
-	pca = a/pow(10, nrca-1); // prima cifra a lui a
+	nra = log10(a) + 1; // nr. cifre a lui a
+	na = 0;
+	// calculez cel mai mic numar de lungime nra
+	// care are toate cifrele egale cu 1 (111...111 (de nra ori 1))
+	for (i = 1; i <= nra; i++)
+		na = na*10 + 1;
 	// calculez cel mai mic numar >= a 
 	// care are toate cifrele egale
-	for (xa = i = 0; i < nrca; i++)
-		xa = xa*10 + pca;
-	if (xa < a)
+	for (i = 1; i <= 9; i++)
+		if (na*i >= a) break;
+	while (na*i <= b)
 	{
-		if (pca != 9)
-			for (xa = i = 0; i < nrca; i++)
-				xa = xa*10 + pca+1;
-		else
-			for (xa = i = 0; i <= nrca; i++)
-				xa = xa*10 + 1;
+		// afisez numarul curent care indeplineste conditia
+		fout << na*i << ' ';
+		// calculez urmatorul numarul
+		if (i == 9) na = na*10 + 1, i = 1;
+		else i++;
 	}
-	nrcb = nrCifre(b); // retin nr. cifre a lui b
-	pcb = b/pow(10, nrcb-1); // retin prima cifra a lui b
-	// calculez cel mai mare numar <= b
-	// care are toate cifrele egale
-	for (xb = i = 0; i < nrcb; i++)
-		xb = xb*10 + pcb;
-	if (xb > b)
-	{
-		if (pcb != 1)
-			for (xb = i = 0; i < nrcb; i++)
-				xb = xb*10 + pcb-1;
-		else
-			for (xb = i = 0; i < nrcb-1; i++)
-				xb = xb*10 + 9;
-	}
-	cif = xa%10, nrc = nrca;
-	while (1)
-	{
-		for (i = 0; i < nrc; i++) // afisez numarul curent
-			fout << cif;
-		fout << ' ';
-		// verific daca mai sunt numere din [a, b]
-		// care sa indeplineasca conditia
-		if (cif == xb%10 && nrc == nrcb) break;
-		// mai sunt numere, deci construiesc urmatorul numar
-		cif = (cif+1)%10;
-		if (!cif) cif = 1, nrc++;
-	}
-	fout.close();
 	return 0;
-}
-int nrCifre(int x)
-{
-	int nrc = 0;
-	while (x)
-	{
-		nrc++;
-		x /= 10;
-	}
-	return nrc;
-}
+}	
